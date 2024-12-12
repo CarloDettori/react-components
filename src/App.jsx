673307414251970e3b11480cfdc9f4ff6ebb710a
CarloDettori <dettoric07@gmail.com>
2024-12-12 19:29:05 +0100

    added card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Header from './components/headerController'
 import Footer from './components/footerController'
-
+import Card from './components/cardController'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
+      <Card />
       <Footer />
     </>
   )
---
+++ src/components/cardController.jsx
@@ -0,0 +1,18 @@
+"use strict"
+
+function Card(title, text) {
+    return (
+        <div>
+            <img src="../public/fff.png" alt="600x400img" />
+            <div>
+                <h3>Titolo del post</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias iure nesciunt corporis pariatur architecto quo et nam ex perferendis, minus id velit facere quis voluptatem explicabo cum facilis, incidunt officia.</p>
+            </div>
+        </div >
+    )
+}
+
+
+
+
+export default Card;
