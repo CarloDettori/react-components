d74349a9656f0138df94a74781358cd24e66b613
CarloDettori <dettoric07@gmail.com>
2024-12-12 16:58:46 +0100

    adding resources

---
+++ src/data/post.js
@@ -0,0 +1,36 @@
+"use strict"
+const posts = [
+    { conteggio: 5 },
+    {
+        title: "Ciambellone",
+        content: "Il ciambellone è buono",
+        image: "/public/img/ciambellone.jpeg",
+        tags: ["dessert", "dolce", "feste"],
+    },
+    {
+        title: "Cracker di barbabietola",
+        content: "Il cracker di barbabietola è buono",
+        image: "/public/img/cracker-barbabietola.jpg",
+        tags: ["snak", "dolce", "salutare"],
+    },
+    {
+        title: "Pane fritto dolce",
+        content: "Il pane fritto dolce è buono",
+        image: "/public/img/pane-fritto-dolce.jpg",
+        tags: ["contorno", "salato", "cena"],
+    },
+    {
+        title: "Pasta alla barbabietola",
+        content: "La pasta alla barbabietola è buona",
+        image: "/public/img/pasta-barbabietola.jpg",
+        tags: ["primo", "salto", "pranzo"],
+    },
+    {
+        title: "Torta paesana",
+        content: "La torta paesana è buona",
+        image: "/public/img/paesana.jpg",
+        tags: ["dessert", "dolce", "tradizionale"],
+    }
+]
+
+module.exports = posts;
